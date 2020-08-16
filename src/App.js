84896740bc46ba405889f796825a00e86c0d91eb
Ming Chai <ming.chai4@gmail.com>
2020-08-16 15:33:17 -0700

    add fetch for info and ability to pass state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Header from './components/ui/Header';
+import PokeGrid from './components/ui/PokeGrid'
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchItems() {
+      const result = await axios(`https://pokeapi.co/api/v2/pokemon`);
+      // console.log(result.data);
+      setItems(result.data.results);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <PokeGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
